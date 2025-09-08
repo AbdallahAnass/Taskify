c39d3f60979d75a3fdb0131b213e00d4e1cbc008
AbdallahAnass <anasabdalla99@gmail.com>
2025-09-08 15:13:37 +0300

    Added basic classes

---
+++ js/Task.js
@@ -0,0 +1,9 @@
+export default class Task {
+  constructor(id, title, description, dueDate, isCompleted) {
+    this.id = id;
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.isCompleted = isCompleted;
+  }
+}
---
+++ js/TaskList.js
@@ -0,0 +1,7 @@
+import Task from "./Task.js";
+
+export default class TaskList {
+  constructor() {
+    this.list = [];
+  }
+}
