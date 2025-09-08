dd23d8a9f3736a38aeb041e82c84dd86d0299f63
AbdallahAnass <anasabdalla99@gmail.com>
2025-09-08 18:36:41 +0300

    Add task feature

--- js/Task.js
+++ js/Task.js
@@ -1,9 +1,47 @@
 export default class Task {
   constructor(id, title, description, dueDate, isCompleted) {
-    this.id = id;
-    this.title = title;
-    this.description = description;
-    this.dueDate = dueDate;
-    this.isCompleted = isCompleted;
+    this._id = id;
+    this._title = title;
+    this._description = description;
+    this._dueDate = dueDate;
+    this._isCompleted = isCompleted;
+  }
+
+  // Getters
+  get title() {
+    return this._title;
+  }
+
+  get description() {
+    return this._description;
+  }
+
+  get dueDate() {
+    return this._dueDate;
+  }
+
+  get isCompleted() {
+    return this._isCompleted;
+  }
+
+  // Setters
+  set title(title) {
+    this._title = title;
+  }
+
+  set description(description) {
+    if (description == null) {
+      description = "";
+    }
+
+    this._description = description;
+  }
+
+  set dueDate(dueDate) {
+    this._dueDate = dueDate;
+  }
+
+  set isCompleted(isCompleted) {
+    this._isCompleted = isCompleted;
   }
 }
--- js/TaskList.js
+++ js/TaskList.js
@@ -3,5 +3,80 @@ import Task from "./Task.js";
 export default class TaskList {
   constructor() {
     this.list = [];
+    this.lastID = 0;
+  }
+
+  addTask(title, description, dueDate) {
+    // Validating title
+    if (!this.validateTitle(title)) {
+      throw new Error("Invalid title");
+    }
+
+    // Validating the dueDate
+    if (!this.validateDate(dueDate)) {
+      throw new Error("Invalid due date");
+    }
+
+    // Generating a new ID for task
+    let id = this.generateID();
+
+    // Creating a new task
+    let task = new Task(id, title, description, dueDate, false);
+
+    // Adding the task
+    this.list.push(task);
+
+    return true;
+  }
+
+  generateID() {
+    this.lastID += 1;
+
+    return this.lastID;
+  }
+
+  validateTitle(title) {
+    // Checking for empty title
+    if (title == "" || title == null) {
+      return false;
+    }
+
+    // Checking for a duplicated task
+    if (!this.isDuplicated(title)) {
+      return false;
+    }
+
+    return true;
+  }
+
+  isDuplicated(title) {
+    // Iterating over every task
+    for (let task of this.list) {
+      // Checking for a match
+      if (task.title === title) {
+        return false;
+      }
+    }
+
+    // If no match found
+    return true;
+  }
+
+  validateDate(dueDate) {
+    // No due date yet
+    if (dueDate === "notSet") return true;
+
+    // Checking for dates in the past
+    let currentDate = new Date();
+
+    if (
+      dueDate.getFullYear() < currentDate.getFullYear() ||
+      dueDate.getMonth() < currentDate.getMonth() ||
+      dueDate.getDate() < currentDate.getDate()
+    ) {
+      return false;
+    }
+
+    return true;
   }
 }
